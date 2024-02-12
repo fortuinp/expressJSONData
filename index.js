d53ac5917c1c4468964470ec0f2c21a05dab0b7b
fortuinp <pinda.fortuin@gmail.com>
2024-02-12 16:31:30 +0200

    mon

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('hi');
+import express from 'express'
+
+//express app
+const app =express()
+
+//router
+const router=express.Router()
+
+//port
+const port= +process.env.PORT || 4000
+
+//JSON URL
+const dataURL= 'https://fortuinp.github.io/todayPortfolioData/data/'
+
+//Application middleware
+app.use(
+    router
+)
+
+// / => home
+
+router.get('^/$|ejd',(req,res)=>{
+res.json({
+    status:res.statusCode,
+    msg: 'you\'re ekhaya'
+})
+})
+
+// /education
+router.get('/education', async(req,res)=>{
+    //let res=await fetch(dataURL).json() --- long approach
+    // let {education} =await res
+let {education}=await(await fetch
+    (dataURL)).json()
+    res.json({
+        status:res.statusCode,
+        education
+    })
+})
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+}
+)
